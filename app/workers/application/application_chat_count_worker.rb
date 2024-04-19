a2a1cdcaf96e07b431fdfa67fc93ce9c37481d2d
Ahmed Mos`ad <ahmed.mosad555@gmail.com>
2024-04-19 19:19:03 +0200

    Fix SQL query in chat room message count worker

--- app/workers/application/application_chat_count_worker.rb
+++ app/workers/application/application_chat_count_worker.rb
@@ -4,7 +4,7 @@ class Application::ApplicationChatCountWorker
 
   def perform
     apps_to_update_ids = RedisFacade.instance.fetch_and_clear_set_for_key(RedisFacade::APPLICATIONS_WITH_NEW_CHAT_ROOMS_KEY)
-    Application.where(id: apps_to_update_ids).update_all(chats_count: "SELECT COUNT(*) FROM chat_rooms WHERE chat_rooms.application_id = applications.id") if apps_to_update_ids.present?
+    Application.where(id: apps_to_update_ids).update_all("chats_count = (SELECT COUNT(*) FROM chat_rooms WHERE chat_rooms.application_id = applications.id)") if apps_to_update_ids.present?
     self.class.perform_in(30.minutes)
   end
 end
--- app/workers/chat_room/chat_room_message_count_worker.rb
+++ app/workers/chat_room/chat_room_message_count_worker.rb
@@ -6,7 +6,7 @@ class ChatRoom::ChatRoomMessageCountWorker
 
   def perform
     rooms_to_update_ids = RedisFacade.instance.fetch_and_clear_set_for_key(RedisFacade::CHAT_ROOMS_WITH_NEW_MESSAGES_KEY)
-    ChatRoom.where(id: apps_to_update_ids).update_all(messages_count: "SELECT COUNT(*) FROM chat_messages WHERE chat_messages.chat_room_id = chat_rooms.id") if apps_to_update_ids.present?
+    ChatRoom.where(id: rooms_to_update_ids).update_all("messages_count = (SELECT COUNT(*) FROM chat_messages WHERE chat_messages.chat_room_id = chat_rooms.id)") if rooms_to_update_ids.present?
     self.class.perform_in(30.minutes)
   end
 end
