aa148681cba28b97a0e121cab75ec5609d6b5508
Ahmed Mos`ad <ahmed.mosad555@gmail.com>
2024-04-19 12:22:02 +0200

    fixes(message_count_worker): fix wrong sql query

--- app/workers/chat_room/chat_room_message_count_worker.rb
+++ app/workers/chat_room/chat_room_message_count_worker.rb
@@ -5,8 +5,8 @@ class ChatRoom::ChatRoomMessageCountWorker
   sidekiq_options queue: :low
 
   def perform
-    apps_to_update_ids = RedisFacade.instance.fetch_and_clear_set_for_key(RedisFacade::CHAT_ROOMS_WITH_NEW_MESSAGES_KEY)
-    Application.where(id: apps_to_update_ids).update_all(chats_count: "SELECT COUNT(*) FROM chat_rooms WHERE chat_rooms.application_id = applications.id") if apps_to_update_ids.present?
+    rooms_to_update_ids = RedisFacade.instance.fetch_and_clear_set_for_key(RedisFacade::CHAT_ROOMS_WITH_NEW_MESSAGES_KEY)
+    ChatRoom.where(id: apps_to_update_ids).update_all(messages_count: "SELECT COUNT(*) FROM chat_messages WHERE chat_messages.chat_room_id = chat_rooms.id") if apps_to_update_ids.present?
     self.class.perform_in(30.minutes)
   end
 end
